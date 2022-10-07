ec5dbc53b3ecbac8853c9cccfd4c4e5bd9d68e53
Ramadhan Putra <91359996+lanang309@users.noreply.github.com>
2022-10-07 15:35:57 +0700

    Add files via upload

---
+++ tgsjarvis/tugas.php
@@ -0,0 +1,18 @@
+<?php
+
+$star=5;
+for($a=$star;$a>0;$a--){
+for($b=$star;$b>=$a;$b--){
+    echo "*";
+}
+echo "<br>";
+}
+
+
+echo "<br><br>";
+
+ for($i = 3; $i <= 50; $i+= 3){
+     echo " Ini perulangan ke-$i <br>";
+ }
+
+?>
